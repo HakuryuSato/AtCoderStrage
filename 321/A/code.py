babac768acde8571546d29de18122a1df1b6a7b6
HakuryuSato <r700540@gmail.com>
2024-08-07 20:31:46 +0900

    321

---
+++ 321/A/code.py
@@ -0,0 +1,19 @@
+import sys
+
+# ローカル用
+file_number = 4
+sys.stdin = open(f"input{file_number}.txt", "r")
+
+# # 本番用
+# sys.stdin = sys.__stdin__
+
+
+N = input()
+
+nums = list(map(int, N))
+is_yes = all(nums[i] > nums[i + 1] for i in range(len(nums) - 1))
+
+if(is_yes):
+    print('Yes')
+else:
+    print('No')
