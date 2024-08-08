bf68bb54b3c0fccb6e16fda09da15d2bb9b0bde4
HakuryuSato <r700540@gmail.com>
2024-08-08 15:28:53 +0900

    320

---
+++ 320/A/code.py
@@ -0,0 +1,13 @@
+import sys
+
+# ローカル用
+# file_number = 1 
+# sys.stdin = open(f'input{file_number}.txt', 'r')
+
+# # 本番用
+sys.stdin = sys.__stdin__
+
+
+A,B = map(int,input().split())
+
+print(A**B + B**A)
---
+++ 320/B/code.py
@@ -0,0 +1,28 @@
+import sys
+
+# ローカル用
+# file_number = 3
+# sys.stdin = open(f"input{file_number}.txt", "r")
+
+# 本番用
+sys.stdin = sys.__stdin__
+
+
+def is_palindrome(sub):
+    return sub == sub[::-1]
+
+
+def check(S):
+    n = len(S)
+    max_length = 1
+
+    for i in range(n):
+        for j in range(i, n):
+            if is_palindrome(S[i : j + 1]):
+                max_length = max(max_length, j - i + 1)
+    return max_length
+
+
+S = input()
+
+print(check(S))
