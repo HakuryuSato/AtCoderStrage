7dd8a0da987ac6fe8aa6d818501877f81aba60d5
HakuryuSato <r700540@gmail.com>
2024-09-14 22:48:33 +0900

    371

--- 371/D/code.py
+++ 371/D/code.py
@@ -7,13 +7,12 @@ import sys
 # # 提出用
 sys.stdin = sys.__stdin__
 
-N = int(input())  # 村の数 4 (1~2*10^5)
-X = list(map(int, input().split()))  # 座標 1 3 5 7 (-10^9~10^9)
-P = list(map(int, input().split()))  # 村人数 1 2 3 4 (1~10^9)
-Q = int(input())  # クエリ数 4 (1~2*10^5)
+N = int(input()) 
+X = list(map(int, input().split()))  
+P = list(map(int, input().split()))  
+Q = int(input()) 
 
 
-# 座標が大きいので検索用
 def binary_search(arr,target,is_high):
     left,right = 0,len(arr)
 
@@ -29,12 +28,11 @@ def binary_search(arr,target,is_high):
                 left = mid + 1
             else:
                 right = mid
-    # 検索終了
     return left
 
 
 
-# 累積和
+
 villages = sorted(zip(X, P))
 sorted_X = [v[0] for v in villages]
 
@@ -45,14 +43,14 @@ for i in range(1, N + 1):
 
     
 for _ in range(Q): # 2*10^5 = 最大10万
-    L, R = map(int, input().split()) # 検索範囲 座標L以上、R以下の人数総数を求める
+    L, R = map(int, input().split()) 
     left_idx = binary_search(sorted_X, L, True)
     right_idx = binary_search(sorted_X, R, False) - 1
 
-    # 範囲に該当する村が存在する場合、人数の総和を計算
+
     if left_idx <= right_idx:
         total_population = cum_sum[right_idx + 1] - cum_sum[left_idx]
     else:
-        total_population = 0  # 該当する村がない場合
+        total_population = 0 
 
     print(total_population)
