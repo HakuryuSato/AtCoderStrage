331ee93fd169c8312a1226c786ec2c69c5ae3c77
HakuryuSato <r700540@gmail.com>
2024-09-22 11:33:08 +0900

    更新

--- 372/C/code.py
+++ 372/C/code.py
@@ -1,4 +1,4 @@
-
+import sys
 
 # ローカル用
 # file_number = 3
@@ -7,31 +7,44 @@
 # # 提出用
 sys.stdin = sys.__stdin__
 
-import sys
+
 data = sys.stdin.read().split()
 N = int(data[0])
 Q = int(data[1])
 S = list(data[2])
 
 queries = data[3:]
-total = 0
 
-for i in range(N - 2):
-    if S[i] == 'A' and S[i+1] == 'B' and S[i+2] == 'C':
-        total += 1
+def count_abc_in_range(s, start, end):
+    count = 0
+    for i in range(start, min(end, N - 2)):
+        if s[i] == 'A' and s[i + 1] == 'B' and s[i + 2] == 'C':
+            count += 1
+    return count
+
+total = count_abc_in_range(S, 0, N)
 
 for q in range(Q):
-    X = int(queries[2 * q])
+    X = int(queries[2 * q]) - 1
     C = queries[2 * q + 1]
-    pos = X - 1
-    affected = [pos - 2, pos - 1, pos]
-    for i in affected:
-        if 0 <= i <= N - 3:
-            if S[i] == 'A' and S[i+1] == 'B' and S[i+2] == 'C':
-                total -= 1
-    S[pos] = C
-    for i in affected:
-        if 0 <= i <= N - 3:
-            if S[i] == 'A' and S[i+1] == 'B' and S[i+2] == 'C':
-                total += 1
+
+    start = max(0, X - 2)
+    end = min(N, X + 3)
+
+    total -= count_abc_in_range(S, start, end)
+
+    S[X] = C
+
+    total += count_abc_in_range(S, start, end)
     print(total)
+
+
+"""
+メモ
+毎回ABCをカウントすると間に合わないので、
+最初にトータルをカウントし、
+変更があったら変更文字の前後6文字を抽出してABCがあるかチェック、
+変更を適用する前後でtotalを計算する。
+計算量はO(Q)？
+
+"""
