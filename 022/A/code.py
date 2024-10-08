7965cd8cb343ad12c190c55d7c4d9e7f6e2e3a80
HakuryuSato <r700540@gmail.com>
2024-10-08 12:56:35 +0900

    022

--- 022/A/code.py
+++ 022/A/code.py
@@ -1,10 +1,20 @@
 import sys
 
 # ローカル用
-file_number = 1 
-sys.stdin = open(f'input{file_number}.txt', 'r')
+# file_number = 2
+# sys.stdin = open(f"input{file_number}.txt", "r")
 
 # # 提出用
-# sys.stdin = sys.__stdin__
+sys.stdin = sys.__stdin__
 
-# 実行時間2秒 メモリ1GB
+
+n, s, t = map(int, input().split())
+w = int(input())
+
+best_count = 1 if s <= w <= t else 0
+
+for _ in range(n - 1):
+    w += int(input())
+    best_count += 1 if s <= w <= t else 0
+
+print(best_count)
