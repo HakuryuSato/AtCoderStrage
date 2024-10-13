11c04b3806355500574fe7983e20e6e2d92d39a8
HakuryuSato <r700540@gmail.com>
2024-10-13 12:02:31 +0900

    004

--- 004/B/code.py
+++ 004/B/code.py
@@ -1,10 +1,20 @@
 import sys
 
 # ローカル用
-file_number = 1 
-sys.stdin = open(f'input{file_number}.txt', 'r')
+# file_number = 1 
+# sys.stdin = open(f'input{file_number}.txt', 'r')
 
 # # 提出用
-# sys.stdin = sys.__stdin__
+sys.stdin = sys.__stdin__
 
 # 実行時間2秒 メモリ1GB
+
+l=[]
+for _ in range(4):
+    l.append(input().split())
+
+rotated = [row[::-1] for row in l[::-1]]
+for row in rotated:
+    print(' '.join(row))
+
+
