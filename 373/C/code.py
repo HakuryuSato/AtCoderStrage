6aa8e102eaf271bd1f2eae2565d86616f6d60dcd
HakuryuSato <r700540@gmail.com>
2024-11-10 11:57:32 +0900

    373

--- 373/C/code.py
+++ 373/C/code.py
@@ -1,14 +1,18 @@
 import sys
 
 # ローカル用
-file_number = 1 
-sys.stdin = open(f'input{file_number}.txt', 'r')
+# file_number = 1
+# sys.stdin = open(f"input{file_number}.txt", "r")
 
 # # 提出用
-# sys.stdin = sys.__stdin__
-
-# 実行時間2秒 メモリ1GB
+sys.stdin = sys.__stdin__
 
 
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
 
+max_A = max(A)
+max_B = max(B)
 
+print(max_A + max_B)
