2c071245a4d62558889178883951b2b40d48dccc
HakuryuSato <r700540@gmail.com>
2024-11-14 12:17:01 +0900

    378

--- 378/A/code.py
+++ 378/A/code.py
@@ -1,9 +1,15 @@
 import sys
+from collections import Counter
 
 # ローカル用
-file_number = 1
-sys.stdin = open(f'input{file_number}.txt', 'r')
+# file_number = 2
+# sys.stdin = open(f'input{file_number}.txt', 'r')
 
 # # 提出用
-# sys.stdin = sys.__stdin__
+sys.stdin = sys.__stdin__
 
+l=list(map(int,input().split()))
+
+counter = Counter(l)
+result = sum(count // 2 for count in counter.values())
+print(result)
