b249e83dcd097c66f0a8525fab3ebd719dc9b2f5
HakuryuSato <r700540@gmail.com>
2024-11-18 12:35:51 +0900

    360

--- 360/C/code.py
+++ 360/C/code.py
@@ -1,36 +1,26 @@
 import sys
+from collections import defaultdict
+
 
 # ローカル用
-file_number = 1
-sys.stdin = open(f"input{file_number}.txt", "r")
+# file_number = 1
+# sys.stdin = open(f"input{file_number}.txt", "r")
 
 # # 本番用
-# sys.stdin = sys.__stdin__
+sys.stdin = sys.__stdin__
 
 N = int(input())
 A = list(map(int, input().split()))
 W = list(map(int, input().split()))
 
-def get_max_box(N,A,W):
-    box_weights = {}
-    for i in range(N):
-        if A[i] not in box_weights:
-            box_weights[A[i]] = 0
-        box_weights[A[i]] += W[i]
-    max_weight = max(box_weights.values())
-    max_weight_box = next((k for k, v in box_weights.items() if v == max_weight), None)
+box_weights = defaultdict(list)
+for a, w in zip(A, W):
+    box_weights[a].append(w)
 
-    return max_weight_box
+move_box_weights = 0
+for weights in box_weights.values():
+    if len(weights) > 1:
+        weights.sort()
+        move_box_weights += sum(weights[:-1])
 
-# 荷物が二つ入っている箱のうち、最小のものを
-
-weight_dict = {}
-for i in range(N):
-    if A[i] not in weight_dict or W[i] < weight_dict[A[i]]:
-        weight_dict[A[i]] = W[i]
-
-total_weight = sum(weight_dict.values())
-print(total_weight)
-
-
-print()
+print(move_box_weights)
