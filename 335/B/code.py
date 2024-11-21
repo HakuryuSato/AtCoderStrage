22665894280daa2f00c98ded78b791af9d2e8237
HakuryuSato <r700540@gmail.com>
2024-11-21 12:03:37 +0900

    335

--- 335/B/code.py
+++ 335/B/code.py
@@ -1,14 +1,14 @@
 import sys
+from itertools import product
 
 # ローカル用
-file_number = 1 
-sys.stdin = open(f'input{file_number}.txt', 'r')
+# file_number = 1
+# sys.stdin = open(f"input{file_number}.txt", "r")
 
 # # 本番用
-# sys.stdin = sys.__stdin__
+sys.stdin = sys.__stdin__
 
-
-
-
-
-
+n = int(input())
+for x, y, z in product(range(n + 1), repeat=3):
+    if x + y + z <= n:
+        print(x, y, z)
