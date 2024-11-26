38bb5ccde77bc1b37cadcda7ac58ef15d55ab7e2
HakuryuSato <r700540@gmail.com>
2024-11-26 11:37:11 +0900

    353

--- 353/B/code.py
+++ 353/B/code.py
@@ -1,11 +1,11 @@
 import sys
 
 # ローカル用
-file_number = 1
-sys.stdin = open(f'input{file_number}.txt', 'r')
+# file_number = 3
+# sys.stdin = open(f'input{file_number}.txt', 'r')
 
 # # 提出用
-# sys.stdin = sys.__stdin__
+sys.stdin = sys.__stdin__
 
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
