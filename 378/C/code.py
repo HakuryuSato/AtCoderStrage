d4ca3f34aba77e4792c859e64b2326194b958fa2
HakuryuSato <r700540@gmail.com>
2024-12-20 18:34:00 +0900

    378

--- 378/C/code.py
+++ 378/C/code.py
@@ -1,9 +1,24 @@
 import sys
 
+
 # ローカル用
-file_number = 1
-sys.stdin = open(f'input{file_number}.txt', 'r')
+# file_number = 2
+# sys.stdin = open(f"input{file_number}.txt", "r")
 
 # # 提出用
-# sys.stdin = sys.__stdin__
+sys.stdin = sys.__stdin__
 
+N = int(input())
+A = list(map(int, input().split()))
+
+sub = {}
+B = []
+for i in range(N):
+    if A[i] in sub:
+        B.append(sub[A[i]])
+    else:
+        B.append(-1)
+
+    sub[A[i]] = i+1
+
+print(*B)
