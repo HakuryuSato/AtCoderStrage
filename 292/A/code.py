c032d6dd587f191fc46dc7ed410ca7ab52a08d76
HakuryuSato <r700540@gmail.com>
2024-12-27 19:38:33 +0900

    292

--- 292/A/code.py
+++ 292/A/code.py
@@ -1,14 +1,12 @@
 import sys
 
 # ローカル用
-file_number = 1 
-sys.stdin = open(f'input{file_number}.txt', 'r')
+# file_number = 1 
+# sys.stdin = open(f'input{file_number}.txt', 'r')
 
 # # 本番用
-# sys.stdin = sys.__stdin__
+sys.stdin = sys.__stdin__
 
 
-
-
-
-
+S=input()
+print(S.upper())
