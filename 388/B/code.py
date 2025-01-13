4d4938fdf3ea28c67c8bc07d1e58c55a8b95293e
HakuryuSato <r700540@gmail.com>
2025-01-13 09:30:22 +0900

    388

--- 388/B/code.py
+++ 388/B/code.py
@@ -1,9 +1,19 @@
 import sys
 
 # ローカル用
-file_number = 1
-sys.stdin = open(f'input{file_number}.txt', 'r')
+# file_number = 2
+# sys.stdin = open(f"input{file_number}.txt", "r")
 
 # # 提出用
-# sys.stdin = sys.__stdin__
+sys.stdin = sys.__stdin__
 
+N, D = map(int, input().split())
+T_L = [tuple(map(int, input().split())) for _ in range(N)]
+
+
+for k in range(1, D + 1):
+    max_weight = 0
+    for T, L in T_L:
+        weight = T * (L + k)
+        max_weight = max(weight, max_weight)
+    print(max_weight)
