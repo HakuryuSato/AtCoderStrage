5e23a02ab5469699aeb8c759bb49afc69f230fb7
HakuryuSato <r700540@gmail.com>
2025-01-14 09:50:52 +0900

    388

--- 388/C/code.py
+++ 388/C/code.py
@@ -1,9 +1,20 @@
 import sys
+from bisect import bisect_right
 
 # ローカル用
-file_number = 1
-sys.stdin = open(f'input{file_number}.txt', 'r')
+# file_number = 3
+# sys.stdin = open(f'input{file_number}.txt', 'r')
 
 # # 提出用
-# sys.stdin = sys.__stdin__
+sys.stdin = sys.__stdin__
 
+N=int(input())
+A=list(map(int,input().split()))
+
+res=0
+
+for i in range(1,N):
+    idx = bisect_right(A,A[i]/2)
+    res+=idx
+
+print(res)
