0e0ce125296cd2c98829cc2c0e27a20193c99453
HakuryuSato <r700540@gmail.com>
2025-01-15 13:29:12 +0900

    287

--- 287/A/code.py
+++ 287/A/code.py
@@ -1,9 +1,16 @@
 import sys
 
 # ローカル用
-file_number = 1
-sys.stdin = open(f'input{file_number}.txt', 'r')
+# file_number = 1
+# sys.stdin = open(f'input{file_number}.txt', 'r')
 
 # # 提出用
-# sys.stdin = sys.__stdin__
+sys.stdin = sys.__stdin__
 
+N=int(input())
+S=[input() for _ in range(N)]
+
+if S.count('For') > S.count('Against'):
+    print('Yes')
+else:
+    print('No')
