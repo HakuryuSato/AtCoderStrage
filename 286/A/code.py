5d975413a9470f0aa5d96996e1a15e1ddac92eac
HakuryuSato <r700540@gmail.com>
2025-01-16 19:05:47 +0900

    286

---
+++ 286/A/code.py
@@ -0,0 +1,15 @@
+import sys
+
+# ローカル用
+file_number = 1
+sys.stdin = open(f"input{file_number}.txt", "r")
+
+# # 提出用
+# sys.stdin = sys.__stdin__
+
+N, P, Q, R, S = map(int, input().split())
+
+A = list(map(int, input().split()))
+A[P - 1 : Q], A[R - 1 : S] = A[R - 1 : S], A[P - 1 : Q]
+
+print(*A)
