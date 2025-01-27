520de92dd5140b3e913ab9cba1647cb2517b257c
HakuryuSato <r700540@gmail.com>
2025-01-27 20:11:56 +0900

    327

--- 327/B/code.py
+++ 327/B/code.py
@@ -1,10 +1,26 @@
 import sys
 
 # ローカル用
-file_number = 1 
-sys.stdin = open(f'input{file_number}.txt', 'r')
+# file_number = 3
+# sys.stdin = open(f"input{file_number}.txt", "r")
 
 # # 本番用
-# sys.stdin = sys.__stdin__
+sys.stdin = sys.__stdin__
 
+b = int(input())
 
+if b == 1:
+    print(1)
+    exit()
+
+for a in range(2, 21):
+    temp = b
+    count = 0
+    while temp % a == 0:
+        temp //= a
+        count += 1
+    if temp == 1 and count == a:
+        print(a)
+        exit()
+
+print(-1)
