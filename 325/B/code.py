648944a799d63bc274326293441f148921157b6f
HakuryuSato <r700540@gmail.com>
2025-01-31 11:23:37 +0900

    325

--- 325/B/code.py
+++ 325/B/code.py
@@ -1,22 +1,21 @@
 import sys
 
 # ローカル用
-file_number = 1 
-sys.stdin = open(f'input{file_number}.txt', 'r')
+# file_number = 1 
+# sys.stdin = open(f'input{file_number}.txt', 'r')
 
 # # 本番用
-# sys.stdin = sys.__stdin__
-
-
-# 0時+xiが9~18に含まれるかどうか？ ずれの限界？
+sys.stdin = sys.__stdin__
 
 
 
 n = int(input())
-cnt = [0 for _ in range(24)] # 空の配列作成
-for i in range(0, n): # 0~nまでループ *値の格納用
+cnt = [0] * 24
+
+for i in range(0, n):
     w, x = map(int, input().split())
     cnt[x] += w
+
 ans = 0
 for i in range(24):
     sum = 0
