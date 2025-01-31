b2c67772bd0dd94b91bbd150b7795fd0b90ac0e0
HakuryuSato <r700540@gmail.com>
2025-01-31 12:00:55 +0900

    325

--- 325/B/code.py
+++ 325/B/code.py
@@ -12,16 +12,17 @@ sys.stdin = sys.__stdin__
 n = int(input())
 cnt = [0] * 24
 
-for i in range(0, n):
-    w, x = map(int, input().split())
-    cnt[x] += w
+for i in range(n):
+    w,x = map(int,input().split())
+    cnt[x]=w
 
-ans = 0
+ans=0
 for i in range(24):
-    sum = 0
+    sum=0
     for j in range(9):
-        sum += cnt[(i + j) % 24]
-    ans = max(ans, sum)
+        sum+=cnt[(i+j)%24]
+    ans = max(ans,sum)
+    
 
 print(ans)
 
