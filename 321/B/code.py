19e669c0887c53c9d6460214da25ecb437319076
HakuryuSato <r700540@gmail.com>
2025-02-01 21:31:10 +0900

    321

--- 321/B/code.py
+++ 321/B/code.py
@@ -1,14 +1,25 @@
 import sys
 
-# ローカル用
-file_number = 1 
-sys.stdin = open(f'input{file_number}.txt', 'r')
+# file_number = 2
+# sys.stdin = open(f'input{file_number}.txt', 'r')
+sys.stdin = sys.__stdin__
 
-# # 本番用
-# sys.stdin = sys.__stdin__
-
-
-
-
-
-
+N,X = map(int,input().split())
+A = list(map(int,input().split()))
+A.append(-1)
+for last in range(0,101):
+    B = A.copy()
+    B[N-1] = last
+    sum = 0
+    ma = 0
+    mi = 100
+    for p in B:
+        sum += p
+        ma = max(ma,p)
+        mi = min(mi,p)
+    score = sum - ma - mi
+    if score >= X:
+        print(last)
+        exit()
+    
+print("-1")
