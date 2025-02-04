b3893d1628c971dedccc17377d950d4b489a2742
HakuryuSato <r700540@gmail.com>
2025-02-04 22:44:15 +0900

    391

--- 391/A/code.py
+++ 391/A/code.py
@@ -1,9 +1,24 @@
 import sys
 
 # ローカル用
-file_number = 1
-sys.stdin = open(f'input{file_number}.txt', 'r')
+# file_number = 2
+# sys.stdin = open(f"input{file_number}.txt", "r")
 
 # # 提出用
-# sys.stdin = sys.__stdin__
+sys.stdin = sys.__stdin__
 
+dict = {
+    "N": "S",
+    "E": "W",
+    "S": "N",
+    "W": "E",
+    "NE": "SW",
+    "NW": "SE",
+    "SE": "NW",
+    "SW": "NE",
+}
+
+
+D=input()
+
+print(dict[D])
