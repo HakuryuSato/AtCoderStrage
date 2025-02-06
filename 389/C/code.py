b3b93411cf8f6c472f103808bc09d394bbec6070
HakuryuSato <r700540@gmail.com>
2025-02-06 12:30:49 +0900

    389

--- 389/C/code.py
+++ 389/C/code.py
@@ -1,9 +1,30 @@
 import sys
+from collections import deque
+from itertools import accumulate
+
 
 # ローカル用
-file_number = 1
-sys.stdin = open(f'input{file_number}.txt', 'r')
+# file_number = 1
+# sys.stdin = open(f'input{file_number}.txt', 'r')
 
 # # 提出用
-# sys.stdin = sys.__stdin__
+sys.stdin = sys.__stdin__
 
+Q = int(input())
+l = deque()
+idx = 0
+start = 0
+
+for i in range(Q):
+    query = list(map(int, input().split()))
+    
+    if query[0] == 1:
+        l.append(idx)
+        idx += query[1]
+
+    elif query[0] == 2:
+        start += 1
+
+    elif query[0] == 3:
+        k = query[1] - 1
+        print(l[start + k] - l[start])
