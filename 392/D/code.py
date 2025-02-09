fabf89c04464adc5ceb82bce715b1fe54bd765c5
HakuryuSato <r700540@gmail.com>
2025-02-09 14:19:15 +0900

    392

--- 392/D/code.py
+++ 392/D/code.py
@@ -1,5 +1,5 @@
 import sys
-import numpy as np
+
 
 # ローカル用
 # file_number = 1
@@ -8,9 +8,17 @@ import numpy as np
 # # 提出用
 sys.stdin = sys.__stdin__
 
-def read_values():return map(int, input().split())
-def read_list():return list(read_values())
 
+def read_values():
+    return map(int, input().split())
+
+
+def read_list():
+    return list(read_values())
+
+
+
+import numpy as np
 n = int(input())
 a = list()
 k = list()
@@ -23,12 +31,12 @@ for _ in range(n):  # 200
         add[r[i + 1]] += 1  # add[出目番号]=回数 という配列
     a.append(add)
 
-    ans = 0.0
-
+   
+ans = 0.0
 for i in range(n):
     for j in range(i + 1, n):  # 全サイコロでループ、順序不要なのでi,jだけで良い
         sum = np.dot(a[i], a[j])  # 内積を計算
-        ans = max(sum / (k[i] * k[j]), ans) # 最大値比較
+        ans = max(sum / (k[i] * k[j]), ans)  # 最大値比較
 
 
 print(ans)
