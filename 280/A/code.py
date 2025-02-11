563415234631f385d94cdc08ea459be0be43c883
HakuryuSato <r700540@gmail.com>
2025-02-11 22:14:04 +0900

    280

---
+++ 280/A/code.py
@@ -0,0 +1,18 @@
+import sys
+
+# ローカル用
+# file_number = 1
+# sys.stdin = open(f'input{file_number}.txt', 'r')
+
+# # 提出用
+sys.stdin = sys.__stdin__
+
+def read_values(): return map(int, input().split())
+def read_list(): return list(read_values())
+
+H,W = read_values()
+result = 0
+for _ in range(H):
+    row = list(input())
+    result += row.count("#")
+print(result)
