fdd575f3f2ea0a3a1a15c1aa9b302b97426fab1b
HakuryuSato <r700540@gmail.com>
2025-02-26 19:46:59 +0900

    276

--- 276/A/code.py
+++ 276/A/code.py
@@ -1,9 +1,16 @@
 import sys
 
 # ローカル用
-file_number = 1
-sys.stdin = open(f'input{file_number}.txt', 'r')
+# file_number = 2
+
+# sys.stdin = open(f'input{file_number}.txt', 'r')
 
 # # 提出用
-# sys.stdin = sys.__stdin__
+sys.stdin = sys.__stdin__
 
+n=input().rfind('a')
+
+if n == -1:
+    print(-1)
+else:
+    print(n+1)
