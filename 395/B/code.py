d10c100e7cf03fa9f91eedee112c94669e7b025a
HakuryuSato <r700540@gmail.com>
2025-03-02 12:25:17 +0900

    395

--- 395/B/code.py
+++ 395/B/code.py
@@ -1,25 +1,28 @@
 import sys
 
 # ローカル用
-file_number = 1
-sys.stdin = open(f'input{file_number}.txt', 'r')
+# file_number = 1
+# sys.stdin = open(f'input{file_number}.txt', 'r')
 
 # # 提出用
-# sys.stdin = sys.__stdin__
+sys.stdin = sys.__stdin__
 
 def read_values(): return map(int, input().split())
 def read_list(): return list(read_values())
 
 N=int(input())
 
-G=[['?']*N]*N
-
-G
+G=[['?']*N for _ in range(N)]
 
 for i in range(N):
     for j in range(N):
-        if i <= j:
-            if i %2 == 1:
+
+        if min(i,j,N-i-1,N-j-1)%2==0:
+            G[i][j]="#"
                 
-        elif i > j:
-            continue
+        else:
+            G[i][j]="."
+
+    
+for g in G:
+    print("".join(g))
--- 395/C/code.py
+++ 395/C/code.py
@@ -1,13 +1,36 @@
 import sys
 
 # ローカル用
-file_number = 1
-sys.stdin = open(f'input{file_number}.txt', 'r')
+# file_number = 3
+# sys.stdin = open(f"input{file_number}.txt", "r")
 
 # # 提出用
-# sys.stdin = sys.__stdin__
+sys.stdin = sys.__stdin__
 
-def read_values(): return map(int, input().split())
-def read_list(): return list(read_values())
+def read_values():
+    return map(int, input().split())
 
+def read_list():
+    return list(read_values())
+
+
+
+from collections import defaultdict
+N = int(input())
+A = read_values()
+
+data = defaultdict(list)
+ans=10**6
+
+for idx, a in enumerate(A): # 10^5
+    data[a].append(idx)
+
+for val in data:
+    positions = data[val]
+    if len(positions) >= 2:
+
+        for i in range(len(positions) - 1):
+            ans = min(ans, positions[i+1] - positions[i])
+
+print(ans+1 if ans != 10**6 else '-1')
 
