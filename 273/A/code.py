59e1571c03fb7baa30fffae17e742fe6cf546e99
HakuryuSato <r700540@gmail.com>
2025-03-09 22:02:08 +0900

    273

--- 273/A/code.py
+++ 273/A/code.py
@@ -1,9 +1,18 @@
 import sys
 
 # ローカル用
-file_number = 1 
-sys.stdin = open(f'input{file_number}.txt', 'r')
+# file_number = 2
+# sys.stdin = open(f'input{file_number}.txt', 'r')
 
 # # 提出用
-# sys.stdin = sys.__stdin__
+sys.stdin = sys.__stdin__
 
+n= int(input())
+
+def rec (n):
+    if n == 0:
+        return 1
+    else:
+        return n * rec(n-1)
+
+print(rec(n))
