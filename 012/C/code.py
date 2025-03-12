1728706cf505c002a1f117dfa4eaf5f803e11064
HakuryuSato <r700540@gmail.com>
2025-03-12 20:18:09 +0900

    012

--- 012/C/code.py
+++ 012/C/code.py
@@ -1,7 +1,7 @@
 import sys
 
 # ローカル用
-# file_number = 1
+# file_number = 2
 # sys.stdin = open(f'input{file_number}.txt', 'r')
 
 # # 提出用
@@ -10,10 +10,10 @@ sys.stdin = sys.__stdin__
 def read_values(): return map(int, input().split())
 def read_list(): return list(read_values())
 
-from itertools import combinations
+N=int(input())
+num = 2025-N
 
-n,m = read_values()
-for l in combinations(range(1,m+1),n):
-    print(*l, sep=' ')
-
-    
+for i in range(10):
+    for j in range(10):
+        if i * j == num:
+            print(f'{i} x {j}')
