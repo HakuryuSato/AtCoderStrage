2fb659dde545491bacaf9a44983f1ab09b92e691
HakuryuSato <r700540@gmail.com>
2025-03-25 22:05:43 +0900

    013

--- 013/B/code.py
+++ 013/B/code.py
@@ -1,13 +1,22 @@
 import sys
 
 # ローカル用
-file_number = 1
-sys.stdin = open(f'input{file_number}.txt', 'r')
+# file_number = 3
+# sys.stdin = open(f"input{file_number}.txt", "r")
 
 # # 提出用
-# sys.stdin = sys.__stdin__
-
-def read_values(): return map(int, input().split())
-def read_list(): return list(read_values())
+sys.stdin = sys.__stdin__
 
 
+def read_values():
+    return map(int, input().split())
+
+
+def read_list():
+    return list(read_values())
+
+
+a = int(input())
+b = int(input())
+
+print(min(max(a, b) - min(a, b), min(a, b) + 10 - max(a, b)))
