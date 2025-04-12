05c2aacbf6dfdc3b02746fb9e8cb5212f0ac70ea
HakuryuSato <r700540@gmail.com>
2025-04-12 20:28:53 +0900

    400

--- 400/A/code.py
+++ 400/A/code.py
@@ -1,13 +1,25 @@
 import sys
 
 # ローカル用
-file_number = 1
-sys.stdin = open(f'input{file_number}.txt', 'r')
+# file_number = 2
+# sys.stdin = open(f"input{file_number}.txt", "r")
 
 # # 提出用
-# sys.stdin = sys.__stdin__
+sys.stdin = sys.__stdin__
 
-def read_values(): return map(int, input().split())
-def read_list(): return list(read_values())
 
+def read_values():
+    return map(int, input().split())
+
+
+def read_list():
+    return list(read_values())
+
+
+A = int(input())
+
+if 400 % A == 0:
+    print(int(400/A))
+else:
+    print(-1)
 
