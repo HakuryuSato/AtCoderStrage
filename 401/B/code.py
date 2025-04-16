59899ccd8fada16090695f161cf73a3db4e4608c
HakuryuSato <r700540@gmail.com>
2025-04-16 14:29:14 +0900

    401

--- 401/B/code.py
+++ 401/B/code.py
@@ -1,13 +1,33 @@
 import sys
 
 # ローカル用
-file_number = 1
-sys.stdin = open(f'input{file_number}.txt', 'r')
+# file_number = 1
+# sys.stdin = open(f"input{file_number}.txt", "r")
 
 # # 提出用
-# sys.stdin = sys.__stdin__
-
-def read_values(): return map(int, input().split())
-def read_list(): return list(read_values())
+sys.stdin = sys.__stdin__
 
 
+def read_values():
+    return map(int, input().split())
+
+
+def read_list():
+    return list(read_values())
+
+
+N = int(input())
+
+is_loggedin = False
+ans=0
+
+for _ in range(N):
+    S = input()
+    if S == "login":
+        is_loggedin = True
+    elif S == "logout":
+        is_loggedin = False
+    elif S == "private" and not is_loggedin:
+        ans+=1
+
+print(ans)
