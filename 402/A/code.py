7e43165104a6144eeb3245960930093b96845349
HakuryuSato <r700540@gmail.com>
2025-05-16 19:10:48 +0900

    402

---
+++ 402/A/code.py
@@ -0,0 +1,19 @@
+import sys
+
+# # ローカル用
+# file_number = 1
+# sys.stdin = open(f'input{file_number}.txt', 'r')
+
+# # 提出用
+sys.stdin = sys.__stdin__
+
+def read_values(): return map(int, input().split())
+def read_list(): return list(read_values())
+
+S=input()
+ans=''
+for s in S:
+    if s.isupper():
+        ans+=s
+
+print(ans)
